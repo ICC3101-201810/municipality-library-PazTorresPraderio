8d76c7fe5f1906163970b45509d4aed6cc4135ff
ING <jcfiguero@miuandes.cl>
2018-05-22 18:19:15 -0400

    First commit

---
+++ Lab10/Lab10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassLibrary1.Person person = null;
+            ClassLibrary1.Address addresss = null;
+            ClassLibrary1.Car car = null;
+            ClassLibrary1.Person parent1 = null;
+            ClassLibrary1.Person parent2 = null;
+            Console.WriteLine("Bievenido a Registro Civil");
+            Console.WriteLine("1. Crear Persona");
+            int op1 = Convert.ToInt32(Console.ReadLine());
+            if (op1 == 1)
+            {
+                Console.WriteLine("Ingrese su Nombre :");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese su Apellido :");
+                string apellido = Console.ReadLine();
+                Console.WriteLine("Ingrese su fecha de nacimiento mm/dd/aaaa");
+                System.DateTime dateTime = Convert.ToDateTime(Console.ReadLine());
+                Console.WriteLine("Ingrese Rut");
+                string rut;
+                rut = Convert.ToString(Console.ReadLine());
+                person = new ClassLibrary1.Person(nombre, apellido, dateTime, null, rut, parent1, parent2);
+                Console.WriteLine("Has sido creado con exito");
+                RegistroCivil.AddPersontoDataBase(person);
+                
+
+            }
+
+
+           
+
+
+     
+        }
+    }
+}
---
+++ Lab10/Lab10/RegistroCivil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using ClassLibrary1;
+
+namespace Lab10
+{
+    public static class RegistroCivil
+    {
+        [DllImport("ClassLibrary1.dll")] 
+        private static extern Object ClassLibrary_ (ClassLibrary1.Person person, ClassLibrary1.Car car , ClassLibrary1.Address address);
+        public static List<ClassLibrary1.Person> persona;
+     
+        public static void AddPersontoDataBase(ClassLibrary1.Person person)
+        {
+            persona.Add(person);
+        }
+        
+        
+    }
+}
